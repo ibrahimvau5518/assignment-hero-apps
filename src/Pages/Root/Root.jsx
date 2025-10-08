2c0748a07332c45a0326f532245e4f78c0a6854f
ibrahimvau5518 <jibonm676@gmail.com>
2025-10-08 12:10:00 +0600

    added navbar

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -6,9 +6,9 @@ import Footer from '../../Components/Footer/Footer';
 const Root = () => {
   return (
     <div className='max-w-6xl mx-auto'>
-      {/* <NavBar></NavBar>
-      <Outlet></Outlet>
-      <Footer></Footer> */}
+      <NavBar></NavBar>
+      {/* <Outlet></Outlet> */}
+      {/* <Footer></Footer> */}
     </div>
   );
 };
